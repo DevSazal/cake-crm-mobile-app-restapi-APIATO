ecf74780dd894e8b6239f520247deba7b7c35c1e
DevSazal <sajal.efran@gmail.com>
2021-05-07 13:10:24 +0600

    Upload Seller Logo (image file) for Profile creation API

--- app/Containers/AppSection/Seller/Actions/CreateSellerAction.php
+++ app/Containers/AppSection/Seller/Actions/CreateSellerAction.php
@@ -28,6 +28,23 @@ class CreateSellerAction extends Action
         // $data['trial_ends_at'] = "2021-12-16";
         $data['trial_ends_at'] = \Carbon\Carbon::now()->addMonths(1);
 
+        // TODO: Upload Seller Logo (image file)
+        if(isset($request->logo)){
+            if($request->logo->getClientOriginalName()){
+                    $ext = $request->logo->getClientOriginalExtension();
+                    $file = date('YmdHis').'_'.rand(1,999).'.'.$ext;
+                    $file_path = $request->logo->storeAs('seller-logo', $file, 'public');
+
+
+                }else{
+                    $file_path = NULL;
+                }
+        }else{
+            $file_path = NULL;
+        }
+
+        $data['logo'] = $file_path;
+
         return app(CreateSellerTask::class)->run($data);
     }
 }
